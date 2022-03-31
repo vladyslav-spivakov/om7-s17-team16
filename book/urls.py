ecbdbf17fb9ba64c6465e4c35f6b5e4e3379dd13
Vladyslav <vladyslav.spivakov@gmail.com>
2022-03-31 15:14:48 +0300

    Created filter by author_id with appropriate html page

--- book/urls.py
+++ book/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('book/', views.all_books),
     path('book/<int:id_book>/', views.book_by_id),
+    path('book/author_<int:author_id>/', views.filter_by_author_id),
     
 ]
--- book/views.py
+++ book/views.py
@@ -25,3 +25,23 @@ def book_by_id(request, id_book):
         'book' : book,
     }
     return render(request, 'book_by_id.html', context)
+
+
+def filter_by_author_id(request, author_id):
+    author = models.Author.get_by_id(author_id)
+    if author is None:
+        context = {
+            'no_author': True,
+            'author_id': author_id,
+        }
+    else:
+        books = author.books
+        context = {
+        'no_author' : False,
+        'is_empty' : not bool(books),
+        'books' : books,
+        'author': author,
+        }
+
+
+    return render(request, 'filter_by_author_id.html', context)
